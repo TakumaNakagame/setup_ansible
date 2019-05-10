897ea0b8d3e4028808f163f14e07aeff462cd649
TakumaNakagame <5129906+TakumaNakagame@users.noreply.github.com>
2019-05-10 16:55:13 +0900

    :heaby_plus_sign: add ubuntu setup

---
+++ ubuntu.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get install software-properties-common -y
+sudo apt-add-repository --yes --update ppa:ansible/ansible
+sudo apt-get install ansible -y
+ansible --version
